0c03b70aefd02b34e278f7519683443531feff9f
Alexander Ivanov <sasha2108i@gmail.com>
2023-01-15 00:31:41 +0300

    Complete program

--- 4_films/Program.cs
+++ 4_films/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
-
+using System.Text.RegularExpressions;
 
 namespace consoleApp
 {
@@ -28,58 +28,135 @@ namespace consoleApp
         }
         public void display()
         {
-            Console.WriteLine("Имя        |         Время начала сеанса         |       Цена        |");
-            Console.WriteLine("----------------------------------------------------------------------");
-            for (int i = 0; i < names.Count; i++)
+            int avg = 0;
+            if (names.Count > 0)
             {
-                Console.WriteLine("{0}                      {1}                         {2}        ", names[i], timeOfstart[i], prices[i]);
+                Console.WriteLine("Имя        |         Время начала сеанса         |       Цена        |");
+                Console.WriteLine("----------------------------------------------------------------------");
+                for (int i = 0; i < names.Count; i++)
+                {
+                    Console.WriteLine("{0}                      {1}                         {2}        ", names[i], timeOfstart[i], prices[i]);
+                    avg += prices[i];
+                }
+                avg = avg / prices.Count;
+                Console.WriteLine("Средняя цена: {0}", avg);
+            } else
+            {
+                Console.WriteLine("Список пуст!");
+            }
+        }
+        public void quest()
+        {
+            int avg = 0;
+            if (names.Count > 0)
+            {
+                for (int i = 0; i < names.Count; i++)
+                {
+                    avg += prices[i];
+                }
+                avg = avg / prices.Count; // takes average number
+                Console.WriteLine("Средняя цена: {0}", avg);
+
+                Console.WriteLine("Имя        |         Время начала сеанса         |       Цена        |");
+                Console.WriteLine("----------------------------------------------------------------------");
+                for (int i = 0; i < names.Count; i++)
+                {
+                    if (isValidQuestTime(timeOfstart[i]) && prices[i] > avg)
+                    {
+                        Console.WriteLine("{0}                      {1}                         {2}        ", names[i], timeOfstart[i], prices[i]);
+                    }
+                }
+            } else
+            {
+                Console.WriteLine("Список пуст!");
             }
             
+        }
+        private static bool isValidQuestTime(string time)
+        {
+            Regex checkTime = new Regex("^(?:[1]?[8-9]|2[0-3]):[0-5][0-9]$");
+            return checkTime.IsMatch(time);
         }
     }
     class Program
     {
-        
+
         static void Main(string[] args)
         {
             List<string> filmName = new List<string>();
             List<string> startTime = new List<string>();
             List<int> price = new List<int>();
 
-        FilmClass film = new FilmClass(filmName, startTime, price);
+            FilmClass film = new FilmClass(filmName, startTime, price);
 
             string mode;
             string title;
             string time;
+            string costAcc;
             int cost;
+
+            bool timeCheck;
+            bool costCheck;
 
             while (true)
             {
-                
-                Console.WriteLine("List(L) / Add(A)");
+                time = "";
+                costAcc = "";
+                timeCheck = false;
+                costCheck = false;
+
+                Console.WriteLine("Список(L) / Добавить(A) / Задание(Q)");
+
                 mode = Console.ReadLine();
                 if (mode == "l" || mode == "L")
                 {
                     film.display();
+                }
+                else if (mode == "q" || mode == "Q")
+                {
+                    film.quest();
                 }
                 else if (mode == "a" || mode == "A")
                 {
                     Console.WriteLine("Введите имя фильма: ");
                     title = Console.ReadLine();
                     Console.WriteLine("Введите время начала сеанса: ");
-                    time = Console.ReadLine();
+                    while (!isValidTime(time))
+                    {
+                        time = Console.ReadLine();
+                        if (!isValidTime(time))
+                        {
+                            Console.WriteLine("Введите корректное время!");
+                        }
+                    }
                     Console.WriteLine("Введите стоимость билета: ");
-                    cost = Convert.ToInt32(Console.ReadLine());
+                    while (!isValidCost(costAcc))
+                    {
+                        costAcc = Console.ReadLine();
+                        if (!isValidCost(costAcc))
+                        {
+                            Console.WriteLine("Введите корректную цену!");
+                        }
+                    }
+                    cost = Convert.ToInt32(costAcc);
                     film.add(title, time, cost);
-                } else
+                    Console.WriteLine("Запись добавлена");
+                } 
+                else
                 {
-                    Console.WriteLine("Неправильная команда");
+                    Console.WriteLine("Неправильная команда!");
                 }
-
             }
-
         }
-
-
+        public static bool isValidTime(string time)
+        {
+            Regex checkTime = new Regex("^(?:[01]?[0-9]|2[0-3]):[0-5][0-9]$");
+            return checkTime.IsMatch(time);
+        }
+        public static bool isValidCost(string cost)
+        {
+            Regex checkCost = new Regex("^\\d+$");
+            return checkCost.IsMatch(cost);
+        }
     }
 }
