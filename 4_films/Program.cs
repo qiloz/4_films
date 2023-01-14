c8b58fe5c1b62cc5fcdd00899229d77d9b6056c2
Alexander Ivanov <sasha2108i@gmail.com>
2023-01-14 22:26:04 +0300

    Добавьте файлы проекта.

---
+++ 4_films/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace consoleApp
+{
+    class FilmClass
+    {
+        private List<string> names;
+        private List<string> dateOfstart;
+        private List<string> price;
+
+
+        private string name;
+        private string dateOfStart;
+        private int price;
+
+        public FilmClass(string name, string dateOfStart, int price)
+        {
+            this.name = name;
+            this.dateOfStart = dateOfStart;
+            this.price = price;
+        }
+
+        public void display() {
+            Console.WriteLine("{0}, {1}, {2}", name, dateOfStart, price);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FilmClass films = new FilmClass("name", "01.01.01", 123);
+            films.display();
+        }
+    }
+}
