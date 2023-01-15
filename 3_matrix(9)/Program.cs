7bc464ceefdd48bb4575f83df6bbc864092f4839
Alexander Ivanov <sasha2108i@gmail.com>
2023-01-15 12:51:03 +0300

    feat: create matrix 9

---
+++ 3_matrix(9)/Program.cs
@@ -0,0 +1,68 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace Program
+{
+    class Matrix
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int width;
+            int height;
+
+            Console.WriteLine("Введите длину матрицы: ");
+            width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ширину матрицы: ");
+            height = Convert.ToInt32(Console.ReadLine());
+            
+            int[,] matrix = new int[width,height] ;
+            int[] answerMatrix = new int[height];
+
+
+            for (int i = 0; i < matrix.GetLength(0); i++) // random values
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rnd.Next(100);
+                }
+                Console.WriteLine();
+
+            }
+            int maxValue = -1;
+            int maxIndex = -1;
+            for (int i = 0; i < matrix.GetLength(0); i++) // print
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i == j && matrix[i, j] > maxValue)
+                    {
+                        maxValue = matrix[i, j];
+                        maxIndex = j;
+                    }
+                    Console.Write("{0}\t", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    if (j == maxIndex)
+                    {
+                        answerMatrix[i] = matrix[i, j];
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Максимальное значение: {0}, Индекс: {1}\n",maxValue, maxIndex);
+
+            for (int i = 0; i < answerMatrix.GetLength(0); i++)
+            {
+                Console.Write("{0}\t", answerMatrix[i]);
+            }
+        }
+    }
+}
